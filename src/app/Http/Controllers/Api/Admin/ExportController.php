dc1ccd6323e55a5deb21b384c826f97732e419a6
dev_duybv <duybv@vicdoers.com>
2022-02-18 14:09:33 +0700

    fix: api export contact form

--- src/app/Http/Controllers/Api/Admin/ExportController.php
+++ src/app/Http/Controllers/Api/Admin/ExportController.php
@@ -30,54 +30,65 @@ class ExportController extends ApiController
             );
         }
     }
-    public function export($slug, Request $request)
+    public function export($type, Request $request)
     {
+
         if (config('export_query.auth_middleware.admin.middleware') !== '') {
             $user = $this->getAuthenticatedUser();
             if (Gate::forUser($user)->denies('view', $this->entity)) {
                 throw new PermissionDeniedException();
             }
         }
-        $export_query = $this->repository->findBySlug($slug);
-        if ($export_query == null) {
+        $export_query = $this->repository->getQuery(['type' => $type]);
+
+        if (empty($export_query)) {
             throw new Exception("Dữ liệu không tồn tại");
         }
-        $data_request = $request->all();
+        if ($type == 'contact-form') {
+            if ($request->has('slug')) {
+                $query = $this->repository->getQuery(['type' => $type, 'slug' => $request->slug])->first();
+                $contact_form_id = DB::table('contact_forms')->where('slug', $request->slug)->first()->id;
 
-        $trans = [];
-        if ($request->has('status') && $request->status != '') {
-            $trans += [":status_condition" => "="];
-        } else {
-            $trans += [":status_condition" => "<>"];
-        }
+                if (!empty($query)) {
+                    $data_request = array_merge($request->all(), ['contact_form_id' => $contact_form_id]);
+                    $trans = [];
+                    if ($request->has('status') && $request->status != '') {
+                        $trans += [":status_condition" => "="];
+                    } else {
+                        $trans += [":status_condition" => "<>"];
+                    }
 
-        if ($request->has('from_date') && $request->from_date != '') {
-            $trans += [":from_date_condition" => ">="];
-        } else {
-            $trans += [":from_date_condition" => "<>"];
-        }
+                    if ($request->has('from_date') && $request->from_date != '') {
+                        $trans += [":from_date_condition" => ">="];
+                    } else {
+                        $trans += [":from_date_condition" => "<>"];
+                    }
 
-        if ($request->has('to_date') && $request->to_date != '') {
-            $trans += [":to_date_condition" => "<="];
-        } else {
-            $trans += [":to_date_condition" => "<>"];
-        }
+                    if ($request->has('to_date') && $request->to_date != '') {
+                        $trans += [":to_date_condition" => "<="];
+                    } else {
+                        $trans += [":to_date_condition" => "<>"];
+                    }
 
-        if ($request->has('position') && $request->position != '') {
-            $trans += [":position_condition" => "="];
-        } else {
-            $trans += [":position_condition" => "<>"];
-        }
-        foreach ($data_request as $item => $val) {
-            if ($val != null) {
-                $trans[":" . $item] = $val;
-            } else {
-                $trans[":" . $item] = "''";
+                    if ($request->has('position') && $request->position != '') {
+                        $trans += [":position_condition" => "="];
+                    } else {
+                        $trans += [":position_condition" => "<>"];
+                    }
+                    foreach ($data_request as $item => $val) {
+                        if ($val != null) {
+                            $trans[":" . $item] = $val;
+                        } else {
+                            $trans[":" . $item] = "0";
+                        }
+                    }
+                    $tring_query = strtr($query->query, $trans);
+                } else {
+                    $tring_query = "SELECT * FROM contact_form_values where `contact_form_id` = $contact_form_id";
+                }
+
             }
         }
-
-        $tring_query = strtr($export_query->query, $trans);
-
         try {
             $query = DB::select($tring_query);
         } catch (Exception $e) {
@@ -86,7 +97,8 @@ class ExportController extends ApiController
         if ($query == null) {
             throw new Exception("Dữ liệu không tồn tại");
         }
-        $label = "export_" . $slug . "_" . date('Y_m_d');
+        $label = "export_" . $request->slug . "_" . date('Y_m_d');
         return Excel::download(new Export($query), $label . '.xlsx');
+
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -4,7 +4,6 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
     $api->group(['prefix' => 'admin'], function ($api) {
-        $api->get('export/{slug}', 'VCComponent\Laravel\Export\Http\Controllers\Api\Admin\ExportController@export');
-
+        $api->get('export/{type}', 'VCComponent\Laravel\Export\Http\Controllers\Api\Admin\ExportController@export');
     });
 });
